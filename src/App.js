b440876ac91af0a33452570bdb6c9eb101a26878
BO-DINH <BO-DINH@BO-DINH>
2021-05-17 18:10:54 +0700

    <></>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import TableCol from "./components/TableCol";
 
 function App() {
-  return (
-    <div className="App">
-        <div style={{textAlign:"center",fontSize:"30px",padding:"20px"}}> Column resizable </div>
-      <TableCol></TableCol>
-    </div>
-  );
+    return (
+        <>
+            <div className="App">
+                <div style={{textAlign: "center", fontSize: "30px", padding: "20px"}}> Column resizable</div>
+                <TableCol></TableCol>
+            </div>
+        </>
+
+    );
 }
 
 export default App;
